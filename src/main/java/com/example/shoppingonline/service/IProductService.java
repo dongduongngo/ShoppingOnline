ce3cbe9659085d80015a53407c8fa1ad0b4d3eb4
dongduongngo <0966856230q@gmail.com>
2021-10-04 17:29:29 +0700

    adding service

--- src/main/java/com/example/shoppingonline/service/IProductService.java
+++ src/main/java/com/example/shoppingonline/service/IProductService.java
@@ -1,9 +1,16 @@
 package com.example.shoppingonline.service;
 
+import com.example.shoppingonline.DTO.ProductDto;
 import com.example.shoppingonline.entity.Product;
 
 import java.util.List;
 
 public interface IProductService {
     List<Product> getAllProducts();
+    List<ProductDto> getAllProductDtos();
+
+    ProductDto create(ProductDto productDto);
+    ProductDto update(ProductDto productDto);
+
+    void delete(Long id);
 }
