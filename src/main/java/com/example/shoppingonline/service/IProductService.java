7311bb74d88bc423f80700c50a92db0450c08bb2
dongduongngo <0966856230q@gmail.com>
2021-10-04 17:25:27 +0700

    Create IProductService.java

---
+++ src/main/java/com/example/shoppingonline/service/IProductService.java
@@ -0,0 +1,9 @@
+package com.example.shoppingonline.service;
+
+import com.example.shoppingonline.entity.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> getAllProducts();
+}
