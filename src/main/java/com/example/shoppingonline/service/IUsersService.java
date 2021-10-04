31dec05582e68d44a9244807d9cffd9b87efefb9
dongduongngo <0966856230q@gmail.com>
2021-10-04 17:32:19 +0700

    adding service

---
+++ src/main/java/com/example/shoppingonline/service/IUsersService.java
@@ -0,0 +1,15 @@
+package com.example.shoppingonline.service;
+
+import com.example.shoppingonline.DTO.UserDto;
+import com.example.shoppingonline.entity.Users;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+public interface IUsersService {
+    List<Users> getAllUsers();
+    List<UserDto> getAllUserDtos();
+
+    Users create(Users users);
+    UserDto update(UserDto userDto);
+}
