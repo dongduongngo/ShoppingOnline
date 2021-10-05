1656338cac64461ace52ccb3677d82cfa39b4e67
dongduongngo <0966856230q@gmail.com>
2021-10-05 11:07:28 +0700

    delete test example

--- src/main/java/com/example/shoppingonline/serviceImpl/hello.java
+++
@@ -1,7 +0,0 @@
-package com.example.shoppingonline.serviceImpl;
-
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("hello Hoang day la Duong");
-    }
-}
